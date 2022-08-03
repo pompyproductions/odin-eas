68e35241bb024456147c1d3a46305b97ab4ce2c6
Eren Gazioglu <pompyproduction@gmail.com>
2022-08-04 00:35:35 +0100

    Add grid functionality

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("script.js: flotation device");
+
+const gridContainer = document.querySelector(".grid-container");
+
+for (let i=0; i<=36; i++) {
+    let elem = document.createElement("div");
+    elem.setAttribute("class", "grid-cell");
+    gridContainer.appendChild(elem);
+    console.log("append element");
+}
