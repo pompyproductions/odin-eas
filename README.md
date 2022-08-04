3648b3ca959d9dd75092d59a0e74f4a50f2af7c3
Eren Gazioglu <pompyproduction@gmail.com>
2022-08-04 04:44:31 +0100

    Add freeze functionality

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # odin-eas
 Etch-and-sketch assignment from The Odin Project curriculum.
+
+## TO-DO
+Add RESET functionality, which would remove .colored from all grid cells and unfreeze.
+
+Place the "grid creation" in an actual named function with parameters, so that you can call it in NEW CANVAS.  
+The named function will check the number of cells to decide their size.  
+if column>100 and row>200  
+else if column>50 and row>100  
+else if column>25 and row>50  
+etc etc etc
+
+Optional: add INVERT button, which toggles .colored for all grid cells.
--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 console.log("script.js: flotation device");
 
 const gridContainer = document.querySelector(".grid-container");
+const freezeButton = document.getElementById("freeze-button");
+let isDrawing = true;
+
+const onMouseOver = (event) => {
+    if (isDrawing && event.target.classList.contains("grid-cell")) {
+        event.target.classList.add("colored")
+    };
+}
+const toggleFreeze = () => {
+    isDrawing = !isDrawing;
+    console.log(document.querySelectorAll(".colored"));
+    document.querySelectorAll(".colored").forEach((item) => {
+        item.classList.toggle("frozen");
+    })
+}
+
+gridContainer.addEventListener("mouseover", onMouseOver);
+freezeButton.addEventListener("click", toggleFreeze);
+
 
 for (let i=0; i<=36; i++) {
     let elem = document.createElement("div");
