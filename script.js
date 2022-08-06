e0163bdede880befc0c9bd28fb6fdaa1cc166ec5
Eren Gazioglu <pompyproduction@gmail.com>
2022-08-06 13:24:34 +0100

    Add functionality: close popup

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 console.log("script.js: flotation device");
 
-const gridContainer = document.querySelector(".grid-container");
-const freezeButton = document.getElementById("freeze-button");
-const resetButton = document.getElementById("reset-button");
-let isDrawing = true;
-
 const onMouseOver = (event) => {
     if (isDrawing && event.target.classList.contains("grid-cell")) {
         event.target.classList.add("colored")
@@ -41,7 +36,7 @@ const createCanvas = (row, col) => {
     gridContainer.setAttribute("style", 
     `grid-template-columns:repeat(${col}, ${gridSize}vh); \
     grid-template-rows:repeat(${row}, ${gridSize}vh)`);
-
+    
     // create and append cells
     for (let i=0; i<=row*col; i++) {
         let elem = document.createElement("div");
@@ -50,8 +45,31 @@ const createCanvas = (row, col) => {
     }
 }
 
+const hidePopup = () => {
+    console.log("pressed");
+    popupContainer.classList.toggle("hidden");
+}
+
+
+const gridContainer = document.querySelector(".grid-container");
 gridContainer.addEventListener("mouseover", onMouseOver);
+
+const popupContainer = document.querySelector(".popup-container");
+
+const freezeButton = document.getElementById("freeze-button");
 freezeButton.addEventListener("click", toggleFreeze);
+
+const resetButton = document.getElementById("reset-button");
 resetButton.addEventListener("click", resetCanvas);
+
+const newButton = document.getElementById("new-button");
+newButton.addEventListener("click", hidePopup);
+
+const popupHideButton = document.getElementById("popup-hide-button");
+popupHideButton.addEventListener("click", hidePopup);
+
+
+let isDrawing = true;
+
 
 createCanvas(20, 20);
